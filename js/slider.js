7ba307ecc4beee86dd56bd80af691a37c2c9c186
aldeevsk <59862247+aldeevsk@users.noreply.github.com>
2021-01-29 23:16:12 +0600

    slider script modified with append and prepend

--- js/slider.js
+++ js/slider.js
@@ -1,32 +1,31 @@
 
-
-let track = document.querySelector('.slider-track');
-let step = 0;
-
-let prev = document.querySelector('.prev');
-let next = document.querySelector('.next');
-
-next.addEventListener('click', function Next () {
-    step = step+100;
-    if (track.style.left=='-500%') {
-        track.style.left = '0';
-        step = 0;
-    } else {
-    track.style.left = `-${step}%`;
-    }
-    console.log(step);
-});
-
-prev.addEventListener('click', function Prev () {
-    step = step-100;
-    if (track.style.left == '' || track.style.left >'0') {
-        track.style.left = '-500%';
-        step = 500;
-    } else {
-    track.style.left = '-' + step + '%';
-    }
-    console.log(step);
-});
+(function () {
+    let track = document.querySelector('.track');
+    const prev = document.querySelector('.prev');
+    const next = document.querySelector('.next');
 
 
+    prev.addEventListener('click', () => {
+        track.style.transition = '0s all linear';
+        track.prepend(track.lastElementChild);
+        track.style.left = '-390px';
+        setTimeout(() => {
+            track.style.transition = '0.5s all linear';
+            track.style.left = '0';
+        }, 0);
 
+    })
+
+
+    next.addEventListener('click', () => {
+        track.style.transition = '0.5s all linear';
+        track.style.left = '-390px';
+        setTimeout(() => {
+            track.style.transition = '0s all linear';
+            track.append(track.firstElementChild);
+            track.style.left = '0';
+        }, 500);
+
+    })
+    
+})();
